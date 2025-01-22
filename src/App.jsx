06bd30e432bf703987272e0719c7945bef8553be
David Medina <cosoacount@gmail.com>
2025-01-21 23:39:53 -0600

    Mejorando CEO y estilo

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,19 @@ function App() {
 
   return (
     <>
-      <div className='md:h-screen'>
+      <main className='md:h-screen'>
         <div className='md:h-1/6 flex flex-col justify-center my-10 md:my-0'>
           <h1 className='from-sky-800 to-sky-600 font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-br text-center'>Calculadora IP </h1>
         </div>
-        <div className="flex flex-col md:flex-row mx-3 h-5/6 md:mx-8 gap-5 items-center mb-8">
+        <section className="flex flex-col md:flex-row mx-3 h-5/6 md:mx-8 gap-5 items-center mb-8">
           <Ip ip={ip} setIp={setIp}/>
           <Subred ip={ip} setIp={setIp}/>
-        </div>
-      </div>
-      <div className='p-6 bg-zinc-100 mt-20 text-center flex flex-col justify-center text-sky-600 font-bold'>
+        </section>
+      </main>
+      <footer className='p-6 bg-zinc-100 mt-20 text-center flex flex-col justify-center text-sky-600 font-bold'>
         <a href="https://www.github.com/devcoso" target='_blank'>devcoso</a>
         <p className='text-zinc-600'>&copy; {new Date().getFullYear()}</p>
-      </div>
+      </footer>
     </>
   )
 }
--- src/components/Subred.jsx
+++ src/components/Subred.jsx
@@ -94,29 +94,29 @@ export default function Subred({ip, setIp}) {
 
   return (
     <div className='w-full md:w-1/2 bg-zinc-100 md:h-full gap-3 shadow-lg p-10 space-y-3 flex flex-col justify-between'>
-        <p htmlFor='ip' className='text-sky-600 font-bold text-lg md:text-xl lg:text-2xl text-center'>Crea subredes</p>
+        <h2 className='text-sky-600 font-bold text-lg md:text-xl lg:text-2xl text-center'>Crea subredes</h2>
         <form onSubmit={e => handleCrearSubredes(e)} className="space-y-3">
             <div className="space-y-1">
-                <label htmlFor="mascara" className='text-sky-600 font-bold text-lg md:text-xl lg:text-2xl text-center block'>Máscara de las subredes</label>
+                <label htmlFor="mascara_subred" className='text-sky-600 font-bold md:text-lg lg:text-xl text-center block'>Máscara de las subredes</label>
                 <div className='flex justify-around bg-white'>
                     <p className={`w-1/3 font-bold text-sm md:text-base text-center cursor-pointer ${typeMask === 0 ? 'bg-sky-600 text-white' : 'text-neutral-600'}`} onClick={() => setTypeMask(0)}>bits</p>
                     <p className={`w-1/3 font-bold text-sm md:text-base text-center cursor-pointer ${typeMask === 1 ? 'bg-sky-600 text-white' : 'text-neutral-600'}`}  onClick={() => setTypeMask(1)}>decimal</p>
                     <p className={`w-1/3 font-bold text-sm md:text-base text-center cursor-pointer ${typeMask === 2 ? 'bg-sky-600 text-white' : 'text-neutral-600'}`}  onClick={() => setTypeMask(2)}>hexadecimal</p>
                 </div>
-                <select className="m-auto block text-base md:text-lg text-center" id="mascara" value={valueMask} onChange={e => setValueMask(e.target.value)}>
+                <select className="m-auto block text-base md:text-lg text-center" id="mascara_subred" value={valueMask} onChange={e => setValueMask(e.target.value)}>
                     {Array.from({length: 33 - parseInt(ip.getMaskBinary())}, (_, i) => i  + parseInt(ip.getMaskBinary())).reverse().map((i) => <option key={"ipHosts" + i} value={i}>{IP.mostrarTypeMask(i, typeMask)}</option>)}
                 </select>
             </div>
             <div className="flex justify-between w-full sm:w-2/3 md:w-full lg:w-2/3 m-auto"> 
                 <div>
-                    <label htmlFor="mascara" className='text-sky-600 font-bold text-sm md:text-base text-center'>Host por subred</label>
-                    <select className="m-auto block text-base md:text-lg text-center" id="mascara" value={valueMask} onChange={e => setValueMask(e.target.value)}>
+                    <label htmlFor="mascara_host" className='text-sky-600 font-bold text-sm md:text-base text-center'>Host por subred</label>
+                    <select className="m-auto block text-base md:text-lg text-center" id="mascara_host" value={valueMask} onChange={e => setValueMask(e.target.value)}>
                         {Array.from({length: 33 - parseInt(ip.getMaskBinary())}, (_, i) => i  + parseInt(ip.getMaskBinary())).map((i) => <option key={"ipHosts" + i} value={i}>{(2**(32 - i)-2 > 0) ? Intl.NumberFormat('en-US').format(2**(32 - i)-2) : 0 }</option>)}
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="mascara" className='text-sky-600 font-bold text-sm md:text-base text-center'>Subredes</label>
-                    <select className="m-auto block text-base md:text-lg text-center" id="mascara" value={valueMask} onChange={e => setValueMask(e.target.value)}>
+                    <label htmlFor="mascara_subredes" className='text-sky-600 font-bold text-sm md:text-base text-center'>Subredes</label>
+                    <select className="m-auto block text-base md:text-lg text-center" id="mascara_subredes" value={valueMask} onChange={e => setValueMask(e.target.value)}>
                         {Array.from({length: 33 - parseInt(ip.getMaskBinary())}, (_, i) => i  + parseInt(ip.getMaskBinary())).reverse().map((i) => <option key={"ipHosts" + i} value={i}>{Intl.NumberFormat('en-US').format(2**(i - ip.getMaskBinary()))}</option>)}
                     </select>
                 </div>
